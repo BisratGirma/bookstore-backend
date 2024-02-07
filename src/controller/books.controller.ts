2369f3dab7f060501a76f86313efcc8ed4bfb90c
Bisrat Girma <bisratx@gmail.com>
2024-02-07 06:22:45 +0300

    book store dist and src folder for simple organization

--- src/controller/books.controller.ts
+++ src/controller/books.controller.ts
@@ -1,6 +1,12 @@
 import { getSingleBook } from "../service/books.service";
 
-export async function respondToGetSingleBook(req, res, next) {
+import { Request, Response, NextFunction } from "express";
+
+export async function respondToGetSingleBook(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   try {
     const book = await getSingleBook(req.params.id);
     console.log("book: ", book);
